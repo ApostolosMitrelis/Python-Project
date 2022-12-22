0bda2acd99d9b7f77cde339c2c6360247b6854d0
ApostolosMitrelis <63606056+ApostolosMitrelis@users.noreply.github.com>
2022-12-22 13:49:52 +0200

    Update geniko.py

--- geniko.py
+++ geniko.py
@@ -4,6 +4,7 @@ import scipy as sp
 
 
 def generate_sparse_matrix(dim):
+    #generate random sparse matrix
     A = np.zeros((dim,dim), dtype = float)
 
     for _ in range(10000):
@@ -12,6 +13,7 @@ def generate_sparse_matrix(dim):
     return A 
 
 def generate_b(dim):
+    #generate random b array of 1 dimension
     rng = np.random.default_rng()
     b = rng.integers(low=-100, high=100, endpoint=True, size=(dim,1))     
 
@@ -19,10 +21,9 @@ def generate_b(dim):
 
 def csr_format(A):
     #CSR format
-    a, b = np.nonzero(A)
+    a, col_idx = np.nonzero(A)
 
     data = np.array([])
-    col_idx = b
     row_ptr = np.array([])
 
     start = a[0]
@@ -30,7 +31,7 @@ def csr_format(A):
     row_ptr = np.append(row_ptr, 0)
 
     for i in range(len(a)):
-        data = np.append(data,A[a[i]][b[i]])
+        data = np.append(data, A[a[i]][col_idx[i]])
         if start != a[i]:
             count += 1
             row_ptr = np.append(row_ptr, row_ptr[count-1] + np.count_nonzero(a == count-1))
@@ -40,7 +41,24 @@ def csr_format(A):
 
     return data, col_idx, row_ptr
 
-A = np.array([[3,2,0,0,0,0], [0,2,1,0,0,2], [0,2,1,0,0,0],[0,0,3,2,4,0], [0,4,0,0,1,0], [0,0,0,0,2,3]])
+def csc_format(A):
+    #CSC format
+    A = A.T
+    data, row_idx, col_ptr = csr_format(A)
+    return data, row_idx, col_ptr
+
+def coo_format(A):
+    #COO format
+    row, col = np.nonzero(A)
+
+    data = np.array([])
+
+    for i in range(len(row)):
+        data = np.append(data, A[row[i]][col[i]])
+
+    return data, col, row
+
+#A = np.array([[3,2,0,0,0,0], [0,2,1,0,0,2], [0,2,1,0,0,0],[0,0,3,2,4,0], [0,4,0,0,1,0], [0,0,0,0,2,3]])
 
 # for _ in range(10):
 #     A = generate_sparse_matrix(1000)
@@ -50,10 +68,10 @@ A = np.array([[3,2,0,0,0,0], [0,2,1,0,0,2], [0,2,1,0,0,0],[0,0,3,2,4,0], [0,4,0,
 #         print(A)
 #         break
 
-data, col_idx, row_ptr = csr_format(A)
-B = sp.sparse.csr_matrix((data,col_idx,row_ptr), shape=(6,6)).toarray()
-b = np.array([1,2,3,4,5,6]).reshape(6,1)
-x = sp.sparse.linalg.spsolve(B,b)
-print(B)
-print(b)
-print(x)
+#B = sp.sparse.csc_matrix((data,row_idx,col_ptr), shape=(3,3)).toarray()
+# b = np.array([1,2,3,4,5,6]).reshape(6,1)
+# x = sp.sparse.linalg.spsolve(B,b)
+#print(B)
+# print(b)
+# print(x)
+
